57a088ca155a8a4787f02a751f4dcfe56a8bcd36
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-17 05:27:26 -0500

    sql bears lab solution

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 10, 'M', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, 'M', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 10, 'M', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, 'M', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, 'M', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 10, 'M', 'Brown', 'Calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, 'M', 'Brown', 'Calm', true);
+
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (10, 'M', 'Brown', 'Calm', true);
